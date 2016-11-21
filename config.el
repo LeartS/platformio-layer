13f9e847e83c4ec837072cce2e82baebb8780aaf
Leonardo Donelli <learts92@gmail.com>
2016-11-21 19:25:45 +0100

    Initial commit: setup platformio-mode with irony and company-irony
    * Automatically enable irony, company and company-irony
      in platformio-mode
    * Conditionally enable platformio-mode on c-mode and c++-mode

---
+++ config.el
@@ -0,0 +1,15 @@
+;;; config.el --- platformio layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: learts <learts92@gmail.com>
+;; URL: https://github.com/LeartS/spacemacs-layer-platformio
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(spacemacs|defvar-company-backends irony-mode)
+(spacemacs|defvar-company-backends irony-mode)
---
+++ packages.el
@@ -0,0 +1,59 @@
+;;; packages.el --- platformio layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: learts <learts92@gmail.com>
+;; URL: https://github.com/LeartS/spacemacs-layer-platformio
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst platformio-packages
+  '(
+    company
+    irony
+    company-irony
+    irony-eldoc
+    platformio-mode
+    ))
+
+(defun platformio/init-irony ()
+  (use-package irony
+    :defer t
+    :commands (irony-mode irony-install-server)
+    :init
+    (add-hook 'irony-mode-hook
+              (lambda ()
+                (define-key irony-mode-map [remap completion-at-point]
+                  'irony-completion-at-point-async)
+                (define-key irony-mode-map [remap complete-symbol]
+                  'irony-completion-at-point-async)
+                (irony-cdb-autosetup-compile-options)))
+    ))
+
+(defun platformio/init-company-irony ()
+  (use-package company-irony
+    :defer t
+    :init
+    (push 'company-irony company-backends-irony-mode)))
+
+(defun platformio/init-irony-eldoc ()
+  (use-package irony-eldoc
+    :defer t
+    :init
+    (add-hook 'irony-mode-hook 'irony-eldoc)))
+
+(defun platformio/post-init-company ()
+  (spacemacs|add-company-hook irony-mode))
+
+(defun platformio/init-platformio-mode ()
+  (use-package platformio-mode
+    :commands platformio-conditionally-enable
+    :defer t
+    :init
+    (progn
+      (add-hook 'platformio-mode-hook 'irony-mode)
+      (add-to-list 'auto-mode-alist '("\\.ino" . c-mode))
+      (add-hook 'c++-mode-hook 'platformio-conditionally-enable)
+      (add-hook 'c-mode-hook 'platformio-conditionally-enable))))
